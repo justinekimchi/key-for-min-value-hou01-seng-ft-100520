3a5d89281d981bd4efef4c226bc6e59eafd13410
Justine Manaloto <justine.kim.manaloto@gmail.com>
2020-09-19 02:03:48 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-    min_value=0
-  name_hash.each do |key, value|
-    if value < min_value
-      min_value = name_hash[key]
-    end
-    min_value +=1
+    name_hash.each do |index, value|
   end
-    name_hash[key]
 end
